e6f01334708bd0034f65f9a083a25287fd488ccb
aurielback <pawelsierzputowski3@gmail.com>
2023-02-16 22:22:08 +0100

    moving character

--- scenes/Scene1.js
+++ scenes/Scene1.js
@@ -1,11 +1,19 @@
 class Scene1 extends Phaser.Scene {
+
     constructor(){
         super("bootGame");
     }
+
+    preload(){
+        this.load.image("background", "assets/images/background.png");
+        this.load.image("ship", "assets/images/ship.png");
+        this.load.image("ship2", "assets/images/ship2.png");
+        this.load.image("ship3", "assets/images/ship3.png");
+    };
 
     create() {
         //tak sie tworzy tekst na ekranie
         this.add.text(50, 50, "Ladowanie...");
         this.scene.start("playGame");
-    }
+    };
 }
--- scenes/Scene2.js
+++ scenes/Scene2.js
@@ -4,6 +4,29 @@ class Scene2 extends Phaser.Scene {
     }
 
     create(){
-        this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
+        this.background = this.add.image(0, 0, "background");
+        this.background.setOrigin(0, 0);
+
+        this.ship1 = this.add.image(config.width/2 - 50, config.height/2, "ship");
+        this.ship1.angle += 5;
+
+        this.ship2 = this.add.image(config.width/2, config.height/2, "ship2");
+        this.ship3 = this.add.image(config.width/2 + 50, config.height/2, "ship3");
+
+        this.ship1.angle += 3;
+
+        this.add.text(20, 20, "Playing game", {
+            font: "25px Arial", fill: "yellow"
+        });
+    }
+
+    moveShip(ship, speed){
+        ship.y += speed;
+    }
+
+    update(){
+        this.moveShip(this.ship1, 1);
+        this.moveShip(this.ship2, 2);
+        this.moveShip(this.ship3, 3);
     }
 }
